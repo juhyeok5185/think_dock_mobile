3049e8e98bad4455452852aea1dc2e43506d7ffb
juhyeok5185 <qkrwngur5185@naver.com>
2025-04-04 12:59:13 +0900

    added

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,25 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ..loadRequest(Uri.parse("https://thinkdock.co.kr"));
   }
 
+  // 뒤로가기 버튼 처리
+  Future<bool> _onWillPop() async {
+    if (await _controller.canGoBack()) {
+      // WebView에서 뒤로 갈 수 있으면, 뒤로 가기
+      _controller.goBack();
+      return Future.value(false);  // 기본 동작을 막고, WebView 뒤로가기 수행
+    } else {
+      // WebView에서 뒤로 갈 수 없으면, 기본 앱 뒤로 가기 수행
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: WebViewWidget(controller: _controller),
+      child: WillPopScope(
+        onWillPop: _onWillPop,  // 뒤로가기 처리
+        child: WebViewWidget(controller: _controller),
+      ),
     );
   }
 }
