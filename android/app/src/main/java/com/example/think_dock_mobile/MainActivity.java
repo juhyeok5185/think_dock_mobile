fb57aafc9a153a1762536a4976ac203d93248b4c
juhyeok5185 <qkrwngur5185@naver.com>
2025-04-04 11:27:10 +0900

    added

---
+++ android/app/src/main/java/com/example/think_dock_mobile/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.think_dock_mobile;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://thinkdock.co.kr")); // ✅ 여기 수정
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebViewWidget(controller: _controller),
+    );
+  }
+}
